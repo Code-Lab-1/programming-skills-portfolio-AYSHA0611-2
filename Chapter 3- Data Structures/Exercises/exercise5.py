19ef85eed5de22bc064001f4bfc21263e61d9ce7
Aysha <ayshanver06@gmail.com>
2023-03-29 22:18:16 +0400

    Create exercise5.py

---
+++ Chapter 3- Data Structures/Exercises/exercise5.py
@@ -0,0 +1,10 @@
+Guest=["Sharvari","Julisha","Rachita"]
+print(Guest[0],"Your invited to dinner, please be present")
+
+print(Guest[2],"Your invited to dinner, please be present")
+print(Guest[1],"\nSorry, can't make it to dinner")
+
+del(Guest[1])
+Guest.insert(1,"Abiya")
+print(Guest[1],"Your invited to dinner, please be present")
+print(Guest)
