3440513fed3504ff3f9887fad2acf67b79eadbfd
Aysha <ayshanver06@gmail.com>
2023-03-29 22:20:58 +0400

    Create exercise4.py

---
+++ Chapter 4- Control Flow/Exercises/exercise4.py
@@ -0,0 +1,14 @@
+age = 19
+if age < 2:
+    print("You're a baby")
+elif age < 4:
+    print("You're a todler")
+elif age < 13:
+    print("You're a kid")
+elif age < 20:
+    print("You'ra a teenager")
+elif age < 65:
+    print("You're an Adult")
+else:
+    print("You're an elder")
+    
