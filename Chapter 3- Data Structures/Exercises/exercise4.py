0ecf0295f911e78233610642f06aa194cdb9bedc
Aysha <ayshanver06@gmail.com>
2023-03-29 22:17:49 +0400

    Create exercise4.py

---
+++ Chapter 3- Data Structures/Exercises/exercise4.py
@@ -0,0 +1,4 @@
+Guest=["Sharvari","Julisha","Rachita"]
+print(Guest[0],"Your invited to dinner, please be present")
+print(Guest[1],"Your invited to dinner, please be present")
+print(Guest[2],"Your invited to dinner, please be present")
