c83d52fbf40b2115471f35ec6a12b0a12dadbea0
Aysha <ayshanver06@gmail.com>
2023-03-29 22:19:06 +0400

    Create exercise7.py

---
+++ Chapter 3- Data Structures/Exercises/exercise7.py
@@ -0,0 +1,25 @@
+list=["Korea","Canada","Paris","America"]
+#Orginal order#
+print(list)
+#Alphabetical order#
+print(sorted(list))
+#ORginal order#
+print(list)
+#Reverse order#
+print(sorted(list, reverse=True))
+#Orginal order#
+print(list)
+#Reversed#
+list.reverse()
+print(list)
+#Orginal order#
+list.reverse()
+print(list)
+#Alphabetical order#
+list.sort()
+print(list)
+#Reverse alphabetical#
+list.sort(reverse=True)
+print(list)
+
+
