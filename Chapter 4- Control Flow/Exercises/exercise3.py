05ae06b0e421c190839bb293e67adbc826874b2b
Aysha <ayshanver06@gmail.com>
2023-03-29 22:20:34 +0400

    Create exercise3.py

---
+++ Chapter 4- Control Flow/Exercises/exercise3.py
@@ -0,0 +1,7 @@
+alien_color="green"
+if alien_color=="green":
+    print("player just earned 5 points")
+elif alien_color=="yellow":
+    print("player just earned 10 points")
+else:
+    print("player just earned 15 points")
