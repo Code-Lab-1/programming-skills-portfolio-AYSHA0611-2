4e7d4ecfa12ece7cf6b3c9d266c099d736521d4e
Aysha <ayshanver06@gmail.com>
2023-04-01 13:25:41 +0400

    Create exercise2.py

---
+++ Chapter 5- Dictionaries/Exercises/exercise2.py
@@ -0,0 +1,10 @@
+Glossary={"if else":"if else is used for condition statement",
+          "loop":"Loops are helpful in executing the statements for number of times",
+          "while":"to create a while loop",
+          "for":"to create a for loop",
+          "del":"to delete an object"}
+print(Glossary['if else'])
+print(Glossary['loop'])
+print(Glossary['while'])
+print(Glossary['for'])
+print(Glossary['del'])
