50b90ff12467ca453a2339a03a5f40a58d7c2e3b
Aysha <ayshanver06@gmail.com>
2023-04-01 14:16:22 +0400

    Create exercise3.py

---
+++ Chapter 5- Dictionaries/Exercises/exercise3.py
@@ -0,0 +1,18 @@
+Glossary={"if else":"if else is used for condition statement",
+          "loop":"Loops are helpful in executing the statements for number of times",
+          "while":"to create a while loop",
+          "for":"to create a for loop",
+          "del":"to delete an object",
+          "Int":'the integer number type',
+          "float":'the floating number type',
+          "complex":'the complex number type',
+          "Numbers":'there are three numeric types in python'}
+print(Glossary['if else'])
+print(Glossary['loop'])
+print(Glossary['while'])
+print(Glossary['for'])
+print(Glossary['del'])
+print(Glossary['Int'])
+print(Glossary['float'])
+print(Glossary['complex'])
+print(Glossary['Numbers'])
